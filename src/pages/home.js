92d5d45ecfb4857bdc180ed48e5f2bb68c38710a
haydenwade <hayden.wade@alula.net>
2020-04-06 19:54:51 -0500

    added podcast player

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ export default function HomePage() {
             <Row>
                 <Col className="center">
                     We strive to bring you the most valuable insights into becoming and being a successful software engineer. Our host Hayden Wade has coded until his fingers were sore but most of all
-                    he enjoys mentoring aspiring engineers, speaking at Universities, and petting alpacas in his spare time. Zero Plus One Podcast we will cover everything from how to land your dream job, 
+                    he enjoys mentoring aspiring engineers, speaking at Universities, and petting alpacas in his spare time. Zero Plus One Podcast we will cover everything from how to land your dream job,
                     career advice, tech, and hear extraordinary guests talk about their journey to success.
                 </Col>
             </Row>
@@ -24,6 +24,11 @@ export default function HomePage() {
                     <p>
                         Podcasts will be coming soon on a platform near you! Be sure to follow us on social so you don't miss when the first podcast drops, it's going to be fire!
                     </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="center">
+                    <div id='buzzsprout-large-player-987628'></div>
                 </Col>
             </Row>
             <Row>
